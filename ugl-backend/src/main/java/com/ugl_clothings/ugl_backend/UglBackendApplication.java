69825ab193e94159341d6fe14e1e1890a4ffe35c
Amiru Ushan <152049636+amiruushan@users.noreply.github.com>
2025-07-09 09:18:36 +0530

    added backend file

---
+++ ugl-backend/src/main/java/com/ugl_clothings/ugl_backend/UglBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ugl_clothings.ugl_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UglBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UglBackendApplication.class, args);
+	}
+
+}
---
+++ ugl-backend/src/test/java/com/ugl_clothings/ugl_backend/UglBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ugl_clothings.ugl_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UglBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
